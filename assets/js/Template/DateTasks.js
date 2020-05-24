5142c9d89cc120310b39db733508617af61b9fe2
Ander Murias <amurias@lin3s.com>
2020-05-24 05:47:09 +0200

    Updated DateTask js

--- assets/js/Template/DateTasks.js
+++ assets/js/Template/DateTasks.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {withRouter, useParams} from "react-router-dom";
 import axios from "axios";
+import moment from 'moment';
 
 const getTasks = (type, date) => {
-  console.log(date);
   return axios.get(
     process.env.API_URL + '/api/task/' + type + (date ? '?date=' + date : ''),
     {
@@ -19,6 +19,7 @@ const DateTasks = ({date, location}) => {
   const [pendingTasks, setPendingTasks] = useState([]);
 
   const query = useParams();
+  const currentDate = query.date || moment().format('YYYY-MM-DD');
 
   useEffect(() => {
     getTasks('pending', query.date).then(res => setPendingTasks(res.data));
@@ -47,14 +48,14 @@ const DateTasks = ({date, location}) => {
       <h2>Pending {query.date}</h2>
       <ul>
         {pendingTasks.map(
-          (task, i) => <li key={i} onClick={changeTaskStatus(task, query.date)}>{task.id} - {task.description}</li>)
+          (task, i) => <li key={i} onClick={changeTaskStatus(task,currentDate)}>{task.id} - {task.description}</li>)
         }
       </ul>
       <hr/>
       <h2>Done {query.date}</h2>
       <ul>
         {doneTasks.map(
-          (task, i) => <li key={i} onClick={changeTaskStatus(task, query.date)}>{task.id} - {task.description}</li>)
+          (task, i) => <li key={i} onClick={changeTaskStatus(task)}>{task.id} - {task.description}</li>)
         }
       </ul>
     </>
