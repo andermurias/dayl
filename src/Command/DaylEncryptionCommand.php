146cba9772cbeb36e33cea0169563a7f789a7309
Ander Murias <amurias@lin3s.com>
2020-06-05 17:53:42 +0200

    Implemented command and functionality for enable/disable encryption y decryption

---
+++ src/Command/DaylEncryptionCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Command;
+
+use App\Common\Helper;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DaylEncryptionCommand extends Command
+{
+    protected static $defaultName = 'dayl:encryption';
+
+    private $taskRepository;
+    private $em;
+
+    public function __construct(EntityManagerInterface $_em, TaskRepository $_taskRepository)
+    {
+        $this->em = $_em;
+        $this->taskRepository = $_taskRepository;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+
+        $this
+            ->setDescription('Updates the encryption of the database')
+            ->addArgument('type', InputArgument::OPTIONAL, 'Type: encrypt/decrypt');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $method = $input->getArgument('type');
+
+        if ('decrypt' === $method || 'encrypt' === $method) {
+
+            $tasks = $this->taskRepository->findAll();
+            foreach ($tasks as $task) {
+                switch ($method) {
+                    case 'encrypt':
+                        $task->setDescription(Helper::encrypt($task->getDescription(true)), true);
+                        break;
+                    case 'decrypt';
+                        $task->setDescription(Helper::decrypt($task->getDescription(true)), true);
+                        break;
+                }
+                $this->em->persist($task);
+                $this->em->flush();
+            }
+        }
+
+        $io->success('Your database encryption has been updated');
+
+        return 0;
+    }
+}
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -50,7 +50,7 @@ class Task
      */
     private $user;
 
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
@@ -58,60 +58,63 @@ class Task
     /**
      * @VirtualProperty("description")
      */
-    public function getDescription(): ?string
+    public function getDescription(bool $raw = false) : ?string
     {
-        return Helper::decrypt($this->description);
+        $raw = $_ENV['ENCRYPT'] !== 'false' ? $raw : true;
+
+        return $raw ? $this->description : Helper::decrypt($this->description);
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(string $description, bool $raw = false) : self
     {
-        $this->description = Helper::encrypt($description);
+        $raw = $_ENV['ENCRYPT'] !== 'false' ? $raw : true;
+        $this->description = $raw ? $description : Helper::encrypt($description);
 
         return $this;
     }
 
-    public function getDate(): ?\DateTimeInterface
+    public function getDate() : ?\DateTimeInterface
     {
         return $this->date;
     }
 
-    public function setDate(?\DateTimeInterface $date): self
+    public function setDate(?\DateTimeInterface $date) : self
     {
         $this->date = $date;
 
         return $this;
     }
 
-    public function getStart(): ?\DateTimeInterface
+    public function getStart() : ?\DateTimeInterface
     {
         return $this->start;
     }
 
-    public function setStart(?\DateTimeInterface $start): self
+    public function setStart(?\DateTimeInterface $start) : self
     {
         $this->start = $start;
 
         return $this;
     }
 
-    public function getEnd(): ?\DateTimeInterface
+    public function getEnd() : ?\DateTimeInterface
     {
         return $this->end;
     }
 
-    public function setEnd(?\DateTimeInterface $end): self
+    public function setEnd(?\DateTimeInterface $end) : self
     {
         $this->end = $end;
 
         return $this;
     }
 
-    public function getUser(): ?User
+    public function getUser() : ?User
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    public function setUser(?User $user) : self
     {
         $this->user = $user;
 
