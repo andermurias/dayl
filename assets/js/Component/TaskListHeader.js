0bee94812e795c3e33eb0191cc215feefa018916
Ander Murias <amurias@lin3s.com>
2020-09-04 01:28:02 +0200

    Implemented support for easy select day from calendar

--- assets/js/Component/TaskListHeader.js
+++ assets/js/Component/TaskListHeader.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import moment from 'moment';
 import {useTranslation} from 'react-i18next';
 import classNames from 'classnames/bind';
@@ -12,6 +12,8 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import {DatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -21,12 +23,18 @@ const useStyles = makeStyles((theme) => ({
     textTransform: 'capitalize',
     textAlign: 'center',
     width: '100%',
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
   subtitle: {
     width: '100%',
     textTransform: 'uppercase',
     //    fontSize: '1rem',
     lineHeight: 1.5,
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
   left: {
     textAlign: 'center',
@@ -44,18 +52,29 @@ const useStyles = makeStyles((theme) => ({
     opacity: '.3',
     fontWeight: 'regular',
   },
+  datePicker: {
+    display: 'none',
+  },
 }));
 
 const TaskListHeader = ({currentDate}) => {
   const classes = useStyles();
-
   const theme = useTheme();
   const isSmallOrUp = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const history = useHistory();
   const {t} = useTranslation();
+
+  const [selectedDate, setSelectedDate] = useState(moment(currentDate).format('YYYY-MM-DD'));
+  const [pickerStatus, setPickerStatus] = useState(false);
 
   const prevDate = moment(currentDate).subtract(1, 'day').format('YYYY-MM-DD');
   const nexDate = moment(currentDate).add(1, 'day').format('YYYY-MM-DD');
 
+  const onChangeDate = (date) => {
+    setSelectedDate(date);
+    history.push('/tasks/' + moment(date).format('YYYY-MM-DD'));
+  };
   return (
     <Box>
       <Box mb={4}>
@@ -66,11 +85,27 @@ const TaskListHeader = ({currentDate}) => {
             </IconButton>
           </Grid>
           <Grid container item xs={8} sm={10}>
-            <Typography variant="h5" component="h1" className={classes.title}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <DatePicker
+                label="Date Picker"
+                showTodayButton={true}
+                todayLabel={t('tasks.today')}
+                cancelLabel={t('tasks.cancel')}
+                okLabel={t('tasks.ok')}
+                value={selectedDate}
+                open={pickerStatus}
+                onOpen={() => setPickerStatus(true)}
+                onAccept={() => setPickerStatus(false)}
+                onClose={() => setPickerStatus(false)}
+                onChange={onChangeDate}
+                TextFieldComponent={() => null}
+              />
+            </MuiPickersUtilsProvider>
+            <Typography variant="h5" component="h1" className={classes.title} onClick={() => setPickerStatus(true)}>
               {currentDate.format('dddd')}
             </Typography>
-            <Typography variant="h6" component="h2" className={classes.title}>
-              <span className={classes.titleSecondary}>({currentDate.format(isSmallOrUp ? 'LL' : 'L')})</span>
+            <Typography variant="h6" component="h2" className={classes.title} onClick={() => setPickerStatus(true)}>
+              <span className={classes.titleSecondary}>({moment(currentDate).format(isSmallOrUp ? 'LL' : 'L')})</span>
             </Typography>
           </Grid>
           <Grid container item xs={2} sm={1}>
@@ -85,12 +120,12 @@ const TaskListHeader = ({currentDate}) => {
           <Grid container spacing={1}>
             <Grid container item sm={6} xs={12}>
               <Typography variant="overline" component="h3" className={classNames(classes.subtitle, classes.left)}>
-                {t('tasks.header.week')} {currentDate.format('w')}
+                {t('tasks.header.week')} {moment(currentDate).format('w')}
               </Typography>
             </Grid>
             <Grid container item sm={6} xs={12}>
               <Typography variant="overline" component="h3" className={classNames(classes.subtitle, classes.right)}>
-                {currentDate.startOf('week').format('L')} - {currentDate.endOf('week').format('L')}
+                {moment(currentDate).startOf('week').format('L')} - {moment(currentDate).endOf('week').format('L')}
               </Typography>
             </Grid>
           </Grid>
